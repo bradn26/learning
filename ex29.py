e313a09813c5f8b9cdab159f2a28bce15db4dbe7
Brad Neumyer <brad.neumyer@gmail.com>
2012-05-05 20:49:03 -0700

    a few more exercises

---
+++ ex29.py
@@ -0,0 +1,35 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats! The world is doomed!"
+	
+if people > cats:
+	print "Not many cats! The world is saved!"
+	
+if people < dogs:
+	print "The world is drooled on!"
+	
+if people > dogs:
+	print "The world is dry!"
+	
+dogs += 5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs."
+	
+if people <= dogs:
+	print "People are less than or equal to dogs."
+	
+if people == dogs:
+	print "People are dogs"
+	
+# I think that 'if' is something that takes on the action of the code below it
+
+# 'if' function IF the condition is true, do the indented function. I think this has
+# to do with styling & making it easier to see the functions and what they do?
+
+# not indenting leads to an indent error, for whatever reason.
+
+# changing the variables changes the outputs of the 'if-statement'
